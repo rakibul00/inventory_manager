dbbaf01d4c2057764aace878787c8ce17cdf3e47
Md.Rakibul islam <user@MDRakibul-islam.local>
2025-04-22 14:19:52 +0600

    Update repo with latest changes

--- inventory/views.py
+++ inventory/views.py
@@ -52,18 +52,16 @@ def register_view(request):
 
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+     if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, f'Welcome back, {username}!')
-            return redirect('home')
+            return redirect('home')  # Redirect to the home page after login
         else:
             messages.error(request, 'Invalid username or password.')
-    return render(request, 'inventory/login.html')
-
+     return render(request, 'inventory/login.html')
 @login_required
 def logout_view(request):
     logout(request)
--- inventory_manager/settings.py
+++ inventory_manager/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*w^7#_%bd-&dmorvszc2ma#o3*bo_!*mksft346ijt$eu$ri6+'
-
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
